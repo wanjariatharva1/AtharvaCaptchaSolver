3f5ffce5fdb19c6c984220e89f6a29bc3f02db3c
wanjariatharva1 <111411170+wanjariatharva1@users.noreply.github.com>
2025-07-09 14:22:03 +0530

    Update AWCaptcha.py

--- AWCaptcha.py
+++ AWCaptcha.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-GEMINI_API_KEY = "AIzaSyB28IhNewiq2q3ag7_2m6IOEDtxa_bQqro" 
+GEMINI_API_KEY = "AIzaSyDmwBb-q1sqDDzPI2Kp7cYYZfMtfYFpySc" 
 genai.configure(api_key=GEMINI_API_KEY)
 model = genai.GenerativeModel("gemini-1.5-flash")
 
